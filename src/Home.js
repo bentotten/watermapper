6cab65547c025efbcd344ff89a449e4f1dcb295e
Ben Totten <ttotten@pdx.edu>
2021-05-16 21:19:12 -0700

    Created organizational folders; map now rendering correctly

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,43 @@
 import React from "react"
-import map from "./map.PNG"
-import useFetch from "./Api.js"
+import L from 'leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import marker from './img/map-marker.png'
 
+
+// Future improvement: Pull and dynmically create markers for Gage houses
 
 export default function Home(props) {
+    const location = {
+        lat: 45.5051,
+        lng: -122.6750,
+        zoom: 13,
+    }
+    const myIcon = L.icon({
+        iconUrl: marker,
+        iconSize: [25, 25],
+    })
 
-    let url = 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=01646500,01638500&parameterCd=00060,00065'
 
-    const data = useFetch(url);
-    console.log(`Testing ${JSON.stringify(data)}`)
-    console.log(data)
-    const test = JSON.stringify(data)
+    const position = [location.lat, location.lng]
 
+    const test = [45.6391, -122.7619]
 
     return (
         <>
             <br />
             <h2>Portland Water</h2>
             <br />
-
-            <img src={map} alt='Map of Gage Houses in Portland'></img>
-            <div>{test}</div>
-
+            <Map className="map" center={position} zoom={location.zoom} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+                    url="https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=a25BWEXwxHphmT537wWB"
+                />
+                <Marker position={test} icon={myIcon}>
+                    <Popup>
+                        KC: Put Popup Here
+                    </Popup>
+                </Marker>
+            </Map>
 
         </>
     )
