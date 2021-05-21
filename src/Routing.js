11f3b6cf081e2f994ba71398b9ab54abe880abb5
MatthewCole-PDX <macole@pdx.edu>
2021-05-20 18:14:22 -0700

    chart dropdown

--- src/Routing.js
+++ src/Routing.js
@@ -1,23 +1,41 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import React from 'react';
+import { Navbar, Nav, NavItem, NavDropdown } from 'react-bootstrap';
 import Home from './Home.js';
-import Charts from './Charts.js';
 import Page from './Page.js';
+
+import dummyChart from './img/basic-bar-graph.png';
 
 
 export default function Routing(props) {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <h1 className="navbar-brand">Navbar</h1>
-                    <ul className="navbar ml-auto" id="links">
-                        <li className="nav-link"><Link to="/">Home</Link></li>
-                        <li className="nav-link"><Link to="/charts">Charts</Link></li>
-                        <li className="nav-link"><Link to="/page">Other Page</Link></li>
-                    </ul>
-                </nav>
+            <Navbar inverse collapseOnSelect>
+                        <Navbar.Brand>
+                        <a href="#brand">Portland Water Usage Data Dashboard</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    <Navbar.Collapse>
+                        <Nav>
+                        <NavItem>
+                            <Link to="/">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/page">ReadMe</Link>
+                        </NavItem>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavDropdown eventKey={3} title="Chart" id="basic-nav-dropdown">
+                                <div className="chart">
+                                    <img src={dummyChart} alt="graph" />
+                                </div>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>;  
+                            
                 {/* Switches */}
                 <Switch>
                     <Route path="/home">
@@ -25,9 +43,6 @@ export default function Routing(props) {
                     </Route>
                     <Route path="/page">
                         <Page />
-                    </Route>
-                    <Route path="/charts">
-                        <Charts />
                     </Route>
                     <Route>
                         <Home />
