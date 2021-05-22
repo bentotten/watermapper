6b3f411be15e0daa792f10114f3daaa0e7f3de48
bentotten <59932872+bentotten@users.noreply.github.com>
2021-05-22 13:37:56 -0700

    Fixed routing bug

--- src/Routing.js
+++ src/Routing.js
@@ -21,52 +21,52 @@ export default function Routing(props) {
                     <Home />
                 </Route>
                 <Navbar inverse collapseOnSelect className="navbar">
-                        <div className="h-box">
-                            <Navbar.Brand>
-                                <h1>Portland Water Usage Data Dashboard</h1>
-                            </Navbar.Brand>
-                        </div>
-                        <Navbar.Toggle />
-                    
+                    <div className="h-box">
+                        <Navbar.Brand>
+                            <h1>Portland Water Usage Data Dashboard</h1>
+                        </Navbar.Brand>
+                    </div>
+                    <Navbar.Toggle />
+
                     <Navbar.Collapse>
-                    
+
                         <Nav>
-                        <NavItem className="pull-right">
-                            <h2>
-                                <Link to="/">Home</Link>
-                            </h2>
-                        </NavItem>
-                        <NavItem className="pull-right">
-                            <h2>
-                                <Link to="/page">Gradient</Link>
-                            </h2>
-                        </NavItem>
+                            <NavItem className="pull-right">
+                                <h2>
+                                    <Link to="/">Home</Link>
+                                </h2>
+                            </NavItem>
+                            <NavItem className="pull-right">
+                                <h2>
+                                    <Link to="/page">Gradient</Link>
+                                </h2>
+                            </NavItem>
                         </Nav>
                         <Nav>
-                        <h2>
-                        <NavDropdown className="drop" drop='left' title="Chart" id="basic-nav-dropdown">
-                                <div className="chart">
-                                    <Route>
-                                        <Chart />
-                                    </Route>
-                                    {/*<img id="graph" src={dummyChart} alt="graph"/>*/}
-                                </div>
-                        </NavDropdown>
-                        </h2>
+                            <h2>
+                                <NavDropdown className="drop" drop='left' title="Chart" id="basic-nav-dropdown">
+                                    <div className="chart">
+                                        <Route>
+                                            <Chart />
+                                        </Route>
+                                        {/*<img id="graph" src={dummyChart} alt="graph"/>*/}
+                                    </div>
+                                </NavDropdown>
+                            </h2>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>;
-                  
-                            
+
+
                 {/* Switches */}
                 <Switch>
-                    <Route path="/home">
+                    <Route path="/">
                         <Home />
                     </Route>
                     <Route path="/page">
                         <Page />
                     </Route>
-                    
+
                 </Switch>
             </div>
         </Router>
