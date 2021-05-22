635f47c811c7df453f7e2d2bfe53eadc6e0b5e3b
carojas13 <81881761+carojas13@users.noreply.github.com>
2021-05-22 12:54:44 -0700

    new api parsing method works

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import React from "react"
 import L, { layerGroup } from 'leaflet'
-import { Map, TileLayer, Marker, Popup, Circle, LayersControl, LayerGroup} from 'react-leaflet'
+import { Map, TileLayer, Marker, Circle, Popup, LayersControl, LayerGroup} from 'react-leaflet'
+//import Popup from 'react-leaflet-editable-popup'
 import marker from './img/map-marker.png'
 import gages from './data/sites.json'
 import useFetch from "./Api.js"
 
 export default function Page(props) {
+    let data = useFetch('https://waterservices.usgs.gov/nwis/dv/?format=json&sites=14211820,%2014144700,%20453320122483701,%2014211720,%20453004122510301,%2014206900&period=P1D&siteStatus=active')
+    console.log(data.value);
+    /*
+    data.value.timeSeries.forEach(element =>{
+        console.log(element)
+    })
+    */
+    let url =
+    "https://waterservices.usgs.gov/nwis/dv/?format=json&indent=on&parameterCd=00060&statCd=00003&sites=14211820";
     const startLocation = {
         lat: 45.5051,
         lng: -122.6750,
@@ -30,9 +40,50 @@ export default function Page(props) {
         else
             return 500;
     }
-
-
-
+    /*j
+    function LocationMarker() {
+        const [position, setPosition] = useState(null)
+        const map = useMapEvents({
+          click() {
+            map.locate()
+          },
+          locationfound(e) {
+            setPosition(e.latlng)
+            map.flyTo(e.latlng, map.getZoom())
+          },
+        })
+      
+        return position === null ? null : (
+          <Marker position={position}>
+            <Popup>You are here</Popup>
+          </Marker>
+        )
+      }
+     */ 
+      let getData = async () => {
+        try {
+          let response = await fetch(url);
+          let data = await response.json();
+      
+          console.log(data);
+          console.log(data.value.timeSeries[0].values[0].value[0]);
+      
+          const {
+            value,
+            qualifiers,
+            dateTime
+          } = data.value.timeSeries[0].values[0].value[0];
+      
+          console.log(value);
+          console.log(qualifiers[0]);
+          console.log(dateTime);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      
+      getData();
+      
     const position = [startLocation.lat, startLocation.lng]
     const CSP = [gages[0].longitude, gages[0].latitude]; //COLUMBIA SLOUGH AT PORTLAND OR
     const CRV = [gages[1].longitude, gages[1].latitude] //COLUMBIA RIVER AT VANCOUVER WAA
@@ -47,8 +98,6 @@ export default function Page(props) {
 
     const test = [45.6391, -122.7619]
     const coodinates = [[gages[0].longitude, gages[0].latitude], [gages[1].longitude, gages[1].latitude], [gages[2].longitude, gages[2].latitude], [gages[3].longitude, gages[3].latitude], [gages[4].longitude, gages[4].latitude], [gages[5].longitude, gages[5].latitude]]
-    let data = useFetch('https://waterservices.usgs.gov/nwis/dv/?format=json&sites=14211820,%2014144700,%20453320122483701,%2014211720,%20453004122510301,%2014206900&period=P1D&siteStatus=active')
-    console.log(data);
 
     
     return (
@@ -57,8 +106,11 @@ export default function Page(props) {
             <br />
             <h2>Something Goes Here</h2>
         </div>
-        <Map className="map" center={position} zoom={startLocation
-                .zoom} scrollWheelZoom={true}>
+        <Map 
+            className="map" 
+            center={position} zoom={startLocation.zoom} 
+            scrollWheelZoom={true}
+            >
                 <LayersControl position = "topright">
                     <LayersControl.BaseLayer checked name="Open Street Map Hybrid">
                     <TileLayer
