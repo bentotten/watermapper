7954c380309f4bf80d46255c892cdd8331e25b52
carojas13 <81881761+carojas13@users.noreply.github.com>
2021-05-30 16:33:06 -0700

    asynch api data and typerror problem fixed

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react"
-import L, { layerGroup } from 'leaflet'
+import L, { layerGroup, popup } from 'leaflet'
 import { Map, TileLayer, Marker, Circle, Popup, LayersControl, LayerGroup } from 'react-leaflet'
 //import Popup from 'react-leaflet-editable-popup'
 import marker from './img/map-marker.png'
 import gages from './data/sites.json'
 import useFetch from "./Api.js"
 import Home from './Home.js';
+import axios from 'axios';
 
 export default function Page(props) {
     /*
@@ -14,10 +15,19 @@ export default function Page(props) {
         console.log(element)
     })
     */
+    //gage site coordinate variables
+            let PRA = [0, 0]; //PUDDING RIVER AT AURORA
+            let TRL = [0, 0]; //TUALITIN RIVER NEAR WEST LINN
+            let CSC = [0, 0]; //CRYSTAL SPRINGS CREEK
+            let WRP = [0, 0]; //WILLAMETTE RIVER AT PORTLAND
+            let BCB = [0, 0]; //BEVERTON CREEK AT BEAVERTO
     let url =
         "https://waterservices.usgs.gov/nwis/dv/?format=json&indent=on&parameterCd=00060&statCd=00003&sites=14211820,%2014144700,%2014211315,%2014206900,%2014211550 ";
     let url2 =
-        "https://waterservices.usgs.gov/nwis/dv/?format=json&sites=14211820,%2014144700,%2014211315,%2014206900,%2014211550&siteStatus=all"
+        "https://waterservices.usgs.gov/nwis/dv/?format=json&indent=on&parameterCd=00010&statCd=00003&sites=14211720,%2014211542,%2014207200,%2014202000,%20453004122510301 ";
+    let url3 =
+        "https://waterservices.usgs.gov/nwis/dv/?format=json&sites=14211720,%2014211542,%2014207200,%2014202000,%20453004122510301&parameterCd=00010&siteType=FA-CI&siteStatus=active";
+
     const startLocation = {
         lat: 45.5051,
         lng: -122.6750,
@@ -61,55 +71,147 @@ export default function Page(props) {
         )
       }
      */
+      //temperature api call
+      const [water, setData] = useState(null);
+      useEffect(() => {
+        getData();
+        
+      }, [])
+      async function getData(){
+        try {
+          const response = await axios.get(url2);
+          console.log(response);
+          setData(response.data);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+
+      let obj2 =[{}];
+      if(water){
+        console.log(water);
+        for(let i = 0; i < 5; ++i){
+            obj2[i] ={
+                name: water.value.timeSeries[i].sourceInfo.siteName,
+                site: water.value.timeSeries[i].sourceInfo.siteCode[0].value,
+                lat:  water.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.latitude,
+                long: water.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.longitude,
+                temp: water.value.timeSeries[i].values[0].value[0].value
+            };
+        }
+        console.log(obj2);
+        PRA = [obj2[0].lat, obj2[0].long]; //PUDDING RIVER AT AURORA
+        TRL = [obj2[1].lat, obj2[1].long]; //TUALITIN RIVER NEAR WEST LINN
+        CSC = [obj2[2].lat, obj2[2].long]; //CRYSTAL SPRINGS CREEK
+        WRP = [obj2[3].lat, obj2[3].long]; //WILLAMETTE RIVER AT PORTLAND
+        BCB = [obj2[4].lat, obj2[4].long]; //BEVERTON CREEK AT BEAVERTO
+        console.log(obj2[0].name)
+        console.log(obj2[1].name)
+      }   
+
+      //dicharge api call
+      /*
+      const [discharge] = useState(null);
+      useEffect(() => {
+        getData2();
+        
+      }, [])
+      async function getData2(){
+        try {
+          const response2 = await axios.get(url);
+          console.log(response2);
+          setData(response2.data2);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      */
+    //let obj2 = [[]]
+   // console.log(data.value.timeSeries[0].sourceInfo.siteName)
+   /*
+    for(let i = 0; i < 5; ++i){
+        obj2[i] ={
+            name: water.value.timeSeries[i].sourceInfo.siteName,
+            site: water.value.timeSeries[i].sourceInfo.siteCode[0].value,
+            lat:  water.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.latitude,
+            long: water.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.longitude,
+            temp: water.value.timeSeries[i].values[0].value[0].value
+        };
+    }
+    */
+
+   // console.log(obj2[0].name);
+   /*
     let obj = [{}];
     let getData = async () => {
         try {
-            let response = await fetch(url);
+            let response = await fetch(url2);
             let data = await response.json();
-
-            console.log(data);
-            console.log(data.value.timeSeries[0].values[0].value[0]);
-
-            const {
-                value,
-                qualifiers,
-                dateTime
-            } = data.value.timeSeries[0].values[0].value[0];
             for(let i = 0; i < 5; ++i){
                 obj[i] ={
                     name: data.value.timeSeries[i].sourceInfo.siteName,
                     site: data.value.timeSeries[i].sourceInfo.siteCode[0].value,
                     lat:  data.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.latitude,
-                    long: data.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.longitude
-
+                    long: data.value.timeSeries[i].sourceInfo.geoLocation.geogLocation.longitude,
+                    temp: data.value.timeSeries[i].values[0].value[0].value
                 };
             }
+            console.log(obj[0].name);
+            console.log(WRPS)
             console.log(obj);
-            console.log(value);
-            console.log(qualifiers[0]);
-            console.log(dateTime);
+            setTimeout( 10000);
+
         } catch (error) {
             console.error(error);
         }
     };
-
     getData();
-
+        /*
+            const PRA = [45.23317586, -122.7500933]; //= [obj[0].lat, obj[0].long]; //PUDDING RIVER AT AURORA
+            const TRL = [45.35650877, -122.6850937]; //TUALITIN RIVER NEAR WEST LINN
+            const CSC = [45.4740079, -122.6420388]; //CRYSTAL SPRINGS CREEK
+            const WRP = [45.5175, -122.6691667]; //WILLAMETTE RIVER AT PORTLAND
+            const BCB = [45.5009505, -122.852045]; //BEVERTON CREEK AT BEAVERTO
+            const TRL//= [obj[1].lat, obj[1].long]; //TUALITIN RIVER NEAR WEST LINN
+            const CSC//= [obj[2].lat, obj[2].long]; //CRYSTAL SPRINGS CREEK
+            const WRP//= [obj[3].lat, obj[3].long]; //WILLAMETTE RIVER AT PORTLAND
+            const BCB//= [obj[4].lat, obj[4].long]; //BEVERTON CREEK AT BEAVERTO
+    console.log(PRA);
+    console.log(obj);
+    console.log(Array.isArray(obj));
+    console.log(obj[0].name);
+    let result = obj.map(gage =>({
+        name: gage.name,
+        site: gage.site,
+        lat: gage.lat,
+        long: gage.long,
+        temp: gage.temp
+    }));
+    */
+    //console.log(result);
+    //console.log(water.value.timeSeries[0].sourceInfo.siteName);
     const position = [startLocation.lat, startLocation.lng]
+    /*
+    const WRP = [obj[0].long, obj[0].lat]; //WILLAMETTE RIVER AT PORTLAND OR
+    const CRV = [obj[1].long, obj[1].lat]; //COLUMBIA RIVER AT VANCOUVER WAA
+    const BCL = [obj[2].long, obj[2].lat]; //BRONSON CREEK AT LAIDLAW
+    const CSP = [obj[3].long, obj[3].lat]; //WILLAMETTE RIVER AT PORTLAND OR
+    const BCB = [obj[4].long, obj[4].lat]; //BEAVERTON CREEK AT 170TH AVE BEAVERTON OR
+    const FCP = [obj[5].long, obj[5].lat]; //BEAVERTON CREEK AT 170TH AVE BEAVERTON OR
+    */
     const CSP = [gages[0].longitude, gages[0].latitude]; //COLUMBIA SLOUGH AT PORTLAND OR
     const CRV = [gages[1].longitude, gages[1].latitude] //COLUMBIA RIVER AT VANCOUVER WAA
     const BCL = [gages[2].longitude, gages[2].latitude]; //BRONSON CREEK AT LAIDLAW
-    const WRP = [gages[3].longitude, gages[3].latitude] //WILLAMETTE RIVER AT PORTLAND OR
-    const BCB = [gages[4].longitude, gages[4].latitude] //BEAVERTON CREEK AT 170TH AVE BEAVERTON OR
-    const FCP = [gages[5].longitude, gages[5].latitude] //FANNO CREEK AT 56TH AVE AT PORTLAND OR
+    //const WRP = [gages[3].longitude, gages[3].latitude] //WILLAMETTE RIVER AT PORTLAND OR
+    //const BCB = [gages[4].longitude, gages[4].latitude] //BEAVERTON CREEK AT 170TH AVE BEAVERTON OR
     const mapMarker = L.icon({
         iconUrl: marker,
         iconSize: [25, 25],
     })
-
+    //const WRPS = [obj[0].lat, obj[0].long]; //WILLAMETTE RIVER AT PORTLAND OR
+    //console.log(obj);
     const test = [45.6391, -122.7619]
     const coodinates = [[gages[0].longitude, gages[0].latitude], [gages[1].longitude, gages[1].latitude], [gages[2].longitude, gages[2].latitude], [gages[3].longitude, gages[3].latitude], [gages[4].longitude, gages[4].latitude], [gages[5].longitude, gages[5].latitude]]
-
     return (
         <>
             <Home />
@@ -117,9 +219,9 @@ export default function Page(props) {
                 <LayersControl.Overlay checked name="Stream Discharge">
                     <LayerGroup>
                         <Circle
-                            color={colorChange(gages[0].temperature)}
-                            center={CSP}
-                            pathOptions={{ color: 'green', fillColor: 'green' }}
+                            color={'white'}
+                            center={CRV}
+                            pathOptions={{ color: 'red', fillColor: 'green' }}
                             radius={800}
                         />
                         <Circle
@@ -138,10 +240,60 @@ export default function Page(props) {
                             center={BCB}
                             radius={600}
                         />
+                    </LayerGroup>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name="Tempearture">
+                    <LayerGroup>
+                        <Marker position={PRA} icon={mapMarker}>
+                            <Circle
+                            color={colorChange(obj2[0].temp)}
+                            center={PRA}
+                            radius={900}
+                            >
+                                <Popup>
+                                <div><b>{obj2[0].name}</b></div>
+                                <div>Temp: {obj2[0].temp} C</div>
+                                </Popup>
+                            </Circle>
+                        </Marker>
+                        <Marker position={TRL} icon={mapMarker}>
+                            <Circle
+                            color={obj2[1] && colorChange(obj2[1].temp)}
+                            center={TRL}
+                            radius={900}
+                            >
+                                <Popup>
+                                <div> {obj2[1] && obj2[1].name}</div>
+                                <div> Temp: {obj2[1] && obj2[1].temp}</div>
+                                </Popup>
+                            </Circle>
+                        </Marker>
+                        <Marker position={CSC} icon={mapMarker}>
                         <Circle
-                            center={FCP}
-                            radius={600}
-                        />
+                            color={obj2[2] && colorChange(obj2[2].temp)}
+                            center={TRL}
+                            radius={900}
+                            >
+                                <Popup>
+                                <div> {obj2[2] && obj2[2].name}</div>
+                                <div> Temp: {obj2[2] && obj2[2].temp}</div>
+                                </Popup>
+                            </Circle>
+                        </Marker>
+                        <Marker position={WRP} icon={mapMarker}>
+                        <Circle
+                            color={obj2[3] && colorChange(obj2[3].temp)}
+                            center={TRL}
+                            radius={900}
+                            >
+                                <Popup>
+                                <div> {obj2[3] && obj2[3].name}</div>
+                                <div> Temp: {obj2[3] && obj2[3].temp}</div>
+                                </Popup>
+                            </Circle>
+                        </Marker>
+                        <Marker position={BCB} icon={mapMarker}>
+                        </Marker>
                     </LayerGroup>
                 </LayersControl.Overlay>
             </LayersControl>
