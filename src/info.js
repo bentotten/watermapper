a3b8e8a48360d0cfae40492a0e9c1ddf87f0b5be
carojas13 <81881761+carojas13@users.noreply.github.com>
2021-06-03 21:18:58 -0700

    Create info.js

---
+++ src/info.js
@@ -0,0 +1,23 @@
+import CallApi from "./Api.js"
+import gages from './data/sites.json'
+import tempgages from './data/temp_gage_urls.json'
+
+export default function Info(props) {
+    let test = {}
+
+    console.log('Discharge Gages: ' + gages)
+    for (let el in gages) {
+        test = CallApi(gages[el].daily_url);
+        console.log(test)
+    }
+
+    console.log('Temp Gages: ' + tempgages)
+    for (let el in tempgages) {
+        test = CallApi(tempgages[el].daily_url);
+        console.log(test)
+    }
+
+    return (
+        <div> working </div>
+    )
+}
