12301d2ebc213cdaf5979e3bf7c2479f3dac0462
carojas13 <81881761+carojas13@users.noreply.github.com>
2021-06-03 21:22:01 -0700

    Update info.js

--- src/info.js
+++ src/info.js
@@ -3,17 +3,17 @@ import gages from './data/sites.json'
 import tempgages from './data/temp_gage_urls.json'
 
 export default function Info(props) {
-    let test = {}
+    let info = {}
 
     console.log('Discharge Gages: ' + gages)
     for (let el in gages) {
-        test = CallApi(gages[el].daily_url);
+        info = CallApi(gages[el].daily_url);
         console.log(test)
     }
 
     console.log('Temp Gages: ' + tempgages)
     for (let el in tempgages) {
-        test = CallApi(tempgages[el].daily_url);
+        info = CallApi(tempgages[el].daily_url);
         console.log(test)
     }
 
